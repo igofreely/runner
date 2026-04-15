9c7e4915742ad3b57eb5be689538d35bb9f87dac
igofreely <igofreely@qq.com>
2026-04-15 14:38:57 +0800

    fix: remove duplicate WorkoutPoint class declaration in WorkoutData.kt

--- app/src/main/java/com/heartrunner/app/data/WorkoutData.kt
+++ app/src/main/java/com/heartrunner/app/data/WorkoutData.kt
@@ -8,13 +8,3 @@ data class WorkoutPoint(
     val speed: Float,
     val heartRate: Int
 )
-package com.heartrunner.app.data
-
-data class WorkoutPoint(
-    val timestamp: Long,
-    val latitude: Double,
-    val longitude: Double,
-    val altitude: Double,
-    val speed: Float,
-    val heartRate: Int
-)
